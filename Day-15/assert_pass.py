87a62727a475eca1b673411b2e8cc28e03b3da86
Bharat Dasari <149285215+Bharat-dasari-7@users.noreply.github.com>
2026-09-01 17:09:59 +0530

    assert, patterns

---
+++ Day-15/assert_pass.py
@@ -0,0 +1,17 @@
+# if 10 % 2 == 0:
+#     pass
+
+# for i in range(10):
+#     pass
+
+# def func():
+#     pass
+
+# class Animal:
+#     pass
+
+
+a = 10
+b = 0
+
+assert b > 0, 'b is not equal to 0'
---
+++ Day-15/patterns.py
@@ -0,0 +1,55 @@
+# for i in range(5):
+#     for j in range(5):
+#         print('*',end=" ")
+#     print()
+
+# for i in range(5):
+#     for j in range(5):
+#         print(i,end=" ")
+#     print()
+
+# for i in range(5):
+#     for j in range(5):
+#         print(j,end=" ")
+#     print()
+
+# for i in range(5):
+#     for j in range(5):
+#         print(i+j,end=" ")
+#     print()
+
+# for i in range(5):
+#     for j in range(5):
+#         if j % 2 == 0:
+#             print('0', end=" ")
+#         else:
+#             print('1', end=" ")
+#     print()
+
+# for i in range(5):
+#     for j in range(5):
+#         print(j%2, end=" ")
+#     print()
+
+# for i in range(5):
+#     for j in range(5):
+#         if (i+j) % 2 == 0:
+#             print('0', end=" ")
+#         else:
+#             print('1', end=" ")
+#     print()
+
+# for i in range(5):
+#     for j in range(5):
+#         print((i+j) % 2, end=" ")
+#     print()
+
+# for i in range(5):
+#     for j in range(i+1):
+#         print('*', end=" ")
+#     print()
+
+for i in range(5):
+    for j in range(5-i):
+        print('*', end=" ")
+    print()
