53fae0a3d964809976761a816b5ed82ced2436c2
Bharat Dasari <149285215+Bharat-dasari-7@users.noreply.github.com>
2026-09-05 14:48:39 +0530

    String methods

---
+++ Day-05/strings-part-1.py
@@ -0,0 +1,45 @@
+Python 3.11.0 (main, Oct 24 2022, 18:26:48) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print('STRINGS: ')
+STRINGS: 
+>>> s = ''
+>>> s
+''
+>>> s = 'Madhava'
+>>> s
+'Madhava'
+>>> s = 'Madhava' + 'Govinda'
+>>> s
+'MadhavaGovinda'
+>>> s = 'Thala '
+>>> s * 7
+'Thala Thala Thala Thala Thala Thala Thala '
+>>> s = 'VenuGopal'
+s
+>>> 
+>>> s
+'VenuGopal'
+>>> s[0]
+'V'
+>>> s[-1]
+'l'
+>>> s[:4]
+'Venu'
+>>> s[4:]
+'Gopal'
+>>> s[::-1]
+'lapoGuneV'
+>>> s[::1]
+'VenuGopal'
+>>> s[0:len(s):1]
+'VenuGopal'
+>>> s[0:len(s):2]
+'VnGpl'
+>>> 'V' in s
+True
+>>> 'o' in s
+True
+>>> 'y' not in s
+True
+>>> 'z' in s
+False
