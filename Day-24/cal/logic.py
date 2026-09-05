c18fa3f4a6f3b5d292501fa45a65200c86e2c26c
Bharat Dasari <149285215+Bharat-dasari-7@users.noreply.github.com>
2026-09-05 15:23:29 +0530

    ATM & Calculators programs

---
+++ Day-24/cal/logic.py
@@ -0,0 +1,18 @@
+def add(a, b):
+    print("Addition: ", a+b)
+
+
+def sub(a, b):
+    print("Subtraction: ", a-b)
+
+def mul(a, b):
+    print("Multiplication: ", a*b)
+
+def div(a, b):
+    print("Divison: ", a/b)
+
+def expo(a, b):
+    print("Expo: ", a**b)
+
+def rem(a, b):
+    print("Remainder: ", a%b)
---
+++ Day-24/cal/main.py
@@ -0,0 +1,30 @@
+# import logic
+
+# logic.add(2,3)
+# logic.sub(3,2)
+# logic.mul(2,3)
+# logic.div(10,2)
+# logic.expo(3,2)
+# logic.rem(3,2)
+
+
+# import logic as lg
+
+# lg.add(2,3)
+# lg.sub(3,2)
+# lg.mul(2,3)
+# lg.div(10,2)
+# lg.expo(3,2)
+# lg.rem(3,2)
+
+# from logic import add,sub
+# add(2,3)
+# sub(3,2)
+
+from logic import *
+add(2,3)
+sub(3,2)
+mul(2,3)
+div(10,2)
+expo(3,2)
+rem(3,2)
